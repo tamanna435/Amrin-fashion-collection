35a6d9dbbc8fc30691cabe6e3942143d76505695
tamanna435 <tamannamondal435@gmail.com>
2025-06-11 17:32:45 +0530

    Add files via upload

---
+++ Amrin-fashion-collection/src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Amrin Fashion Collection</h1>
+      <p>Your fashion destination for Men, Women & Kids.</p>
+    </div>
+  );
+}
